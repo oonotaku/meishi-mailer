998b92945fca6ed21a8a96e833625f9a8b684faa
oonotaku <taku_oono@node-bee.com>
2026-05-08 16:34:01 +0900

    fix: extracted_snsのキーからsns_プレフィックスを除去

--- pages/api/profile/public.js
+++ pages/api/profile/public.js
@@ -27,7 +27,7 @@ export default async function handler(req, res) {
 
   const extracted_sns = {}
   for (const key of SNS_KEYS) {
-    if (profile[key]) extracted_sns[key] = profile[key]
+    if (profile[key]) extracted_sns[key.replace('sns_', '')] = profile[key]
   }
 
   res.json({
